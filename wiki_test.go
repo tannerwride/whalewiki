5b1439dd61a94991eeff28d2a6129ed44429931d
tannerwride <tanner@circleci.com>
2022-08-17 17:05:58 -0600

    added a dummy test

--- wiki_test.go
+++ wiki_test.go
@@ -5,11 +5,12 @@ import (
 )
 
 func TestPage(t *testing.T) {
-	page := &Page{Title: "Test Title", Body: "Here we go!"}
-	got := page.Body
-	want := "Here we go!"
+	page := &Page{Title: "Test Title", Body: []byte("Here we go!")}
+	got := page.Title
+	want := "Test Title"
 
 	if got != want {
+		t.Errorf("got %q want %q", got, want)
 
 	}
 }
