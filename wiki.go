794e11ad8eaeaa39359975165624b7fafc1d19a6
tannerwride <tanner@circleci.com>
2022-08-17 16:42:57 -0600

    finished the general setup of wiki save and load

--- wiki.go
+++ wiki.go
@@ -9,3 +9,24 @@ type Page struct {
 	Title string
 	Body []byte
 }
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	f1 := &Page{Title: "Test", Body: []byte("Sample page, ok?")}
+	f1.save()
+	f2, _ := loadPage("Test")
+	fmt.Println(string(f2.Body))
+}
